02d00a6df4ff0c78d64fa47b147232d64d889443
nimithaajith <nimithaajith16@gmail.com>
2025-08-01 11:02:19 +0530

    Postgresql database+ sql alchemy settings added.

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# PostgreSQL URI - replace with actual DB info
+DATABASE_URL = "postgresql://username:password@localhost:5432/databasename"
+
+engine = create_engine(DATABASE_URL, echo=True)  # echo=True shows SQL logs
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
